52fff0540e71339244d9dce2b251b39b3cbac0a0
carloandreadefilippo <carloandreadefilippo@gmail.com>
2025-11-30 17:38:19 +0800

    Updated Particle System class, moved the system creation from generate_polymers.py into the particle system class.

--- core/particle_system.py
+++ core/particle_system.py
@@ -6,34 +6,73 @@ from .linked_cell_list import LinkedCellList
 
 class ParticleSystem:
 
-    coordsFirstBeads = []
-    patchyBeadsIDs = []         #List of tuples AtomID Bead + Patch to keep track of the IDs for the bonds
+    def __init__(self, par: dict):
 
-    atomID=1
-    molID=1
+        self.par = par
 
-    def __init__(self, Lbox: list[float], max_sigma: float, ntot: int):
-        self.Lbox = Lbox
-        self.LboxHalf = [0.5*L for L in Lbox]
-        self.spheres: list[Sphere] = []*ntot
-        self.atomList = LinkedCellList(Lbox, max_sigma, ntot)
+        self.Lbox = par["Lbox"]
+        self.LboxHalf = [0.5*L for L in self.Lbox]
 
-    def addPolymers(self, npol:int, ns:int, npatch:int, sigma_bead:float, sigma_patch:float, beadType:int, patchType:int):
+        self.npol = int(par["npol"])
+        self.ns = int(par["ns"])
+        self.npatch = int(par["npatch"])
+        self.nsolvent = int(par["nsolvent"])
+        self.ncolloids = int(par["ncolloids"])
 
-        minPolymerDist = sigma_bead
+        self.ntot = self.npol*(self.ns + self.npatch) + self.nsolvent + self.ncolloids
 
-        if npatch > 0:
-            minPolymerDist +=  2. * sigma_patch
+        self.nbonds = self.npol * (self.ns - 1 + self.npatch)
 
-        distBetweenBeads = 1 * sigma_bead #TODO: Define the distance between the beads depending on the chosen potential
+        self.atom_types = 1
+        self.bond_types = 1
 
-        for p in range(npol):
+        self.beadType = 1
 
-            percent = 100 * (p + 1) / npol
-            print(f"\rPolymer {p+1}/{npol}  ({percent:6.2f}%)", end="")
+        self.sigma_bead    = float(par["sigma_bead"])
+        self.sigma_patch   = float(par["sigma_patch"])
+        self.sigma_solvent = float(par["sigma_solvent"])
+        self.sigma_colloid = float(par["sigma_colloid"])
+
+        if self.nsolvent > 0:
+            self.atom_types += 1
+            self.solventType = self.atom_types
+
+        if self.npatch > 0:
+            self.atom_types += 1
+            self.bond_types += 2
+            self.patchType = self.atom_types
+
+        if self.ncolloids > 0:
+            self.atom_types += 1
+            self.colloidType = self.atom_types
+
+        self.max_sigma = max(self.sigma_bead, self.sigma_patch, self.sigma_solvent, self.sigma_colloid)
+
+        self.spheres: list[Sphere] = []*self.ntot
+        self.atomList = LinkedCellList(self.Lbox, self.max_sigma, self.ntot)
+
+        self.coordsFirstBeads = []
+        self.patchyBeadsIDs = []         #List of tuples AtomID Bead + Patch to keep track of the IDs for the bonds
+
+        self.atomID=1
+        self.molID=1
+
+    def addPolymers(self):
+
+        minPolymerDist = self.sigma_bead
+
+        if self.npatch > 0:
+            minPolymerDist +=  2. * self.sigma_patch
+
+        distBetweenBeads = 1 * self.sigma_bead #TODO: Define the distance between the beads depending on the chosen potential
+
+        for p in range(self.npol):
+
+            percent = 100 * (p + 1) / self.npol
+            print(f"\rPolymer {p+1}/{self.npol}  ({percent:6.2f}%)", end="")
 
             #First bead of the polymers
-            ID_start = int(1 + p * (ns + npatch))
+            ID_start = int(1 + p * (self.ns + self.npatch))
 
             #Random coordinate of first bead (not overlapping with other polymers)
             while True:
@@ -60,10 +99,10 @@ class ParticleSystem:
             self.coordsFirstBeads.append(coordFirstBead)
 
             #List of the indexes of patch beads (sorted to have the correct molID)
-            patchyBeads = sorted(np.random.choice(ns, size=npatch, replace=False).tolist())
+            patchyBeads = sorted(np.random.choice(self.ns, size=self.npatch, replace=False).tolist())
 
             #Place the beads
-            for npart in range(ns):
+            for npart in range(self.ns):
 
                 coord = coordFirstBead.copy()
                 coord[2] += distBetweenBeads * npart   
@@ -73,7 +112,7 @@ class ParticleSystem:
                 for ax in range(3):
                     coordPBC[ax] -= self.Lbox[ax] * round(coordPBC[ax] / self.Lbox[ax])
 
-                self.spheres.append(Sphere(sigma_bead, coordPBC, self.atomID, self.molID, beadType))
+                self.spheres.append(Sphere(self.sigma_bead, coordPBC, self.atomID, self.molID, self.beadType))
                 self.atomList.addObjectToList(self.atomID - 1, coordPBC)
 
                 self.atomID += 1
@@ -88,15 +127,15 @@ class ParticleSystem:
                 #Random position of the patch around the bead
                 theta = np.random.rand() * 2. * m.pi
 
-                patchCoord[0] += m.cos(theta) * 0.5 * (sigma_bead + sigma_patch)
-                patchCoord[1] += m.sin(theta) * 0.5 * (sigma_bead + sigma_patch)
+                patchCoord[0] += m.cos(theta) * 0.5 * (self.sigma_bead + self.sigma_patch)
+                patchCoord[1] += m.sin(theta) * 0.5 * (self.sigma_bead + self.sigma_patch)
 
                 patchCoordPBC = [c - hl for c, hl in zip(patchCoord, self.LboxHalf)]
 
                 for ax in range(3):
                     patchCoordPBC[ax] -= self.Lbox[ax] * round(patchCoordPBC[ax] / self.Lbox[ax])
 
-                self.spheres.append(Sphere(sigma_patch, patchCoordPBC, self.atomID, self.molID, patchType))
+                self.spheres.append(Sphere(self.sigma_patch, patchCoordPBC, self.atomID, self.molID, self.patchType))
 
                 self.atomList.addObjectToList(self.atomID - 1, [c - hl for c, hl in zip(patchCoordPBC, self.LboxHalf)])
 
@@ -106,28 +145,27 @@ class ParticleSystem:
 
                 self.atomID += 1
         
-            print()
-        
         self.molID += 1
+        print()
 
 
-    #TODO: merge the two methods
-    def addColloids(self, ncoll:int, sigma_colloid:float, colloidType:int):
+    #TODO: merge addColloids and addSolvent methods
+    def addColloids(self):
 
-        for nc in range(ncoll):
+        for nc in range(self.ncolloids):
 
-            percent = 100 * (nc + 1) / ncoll
-            print(f"\rColloid {nc+1}/{ncoll}  ({percent:6.2f}%)", end="")
+            percent = 100 * (nc + 1) / self.ncolloids
+            print(f"\rColloid {nc+1}/{self.ncolloids}  ({percent:6.2f}%)", end="")
 
             while True:
 
-                colloid = Sphere(sigma_colloid, 
+                colloid = Sphere(self.sigma_colloid, 
                                 (np.random.uniform(-self.LboxHalf[0], self.LboxHalf[0]),
                                 np.random.uniform(-self.LboxHalf[1], self.LboxHalf[1]),
                                 np.random.uniform(-self.LboxHalf[2], self.LboxHalf[2])),
                                 self.atomID,
                                 self.molID,
-                                colloidType)
+                                self.colloidType)
 
                 if self.atomList.overlapCheck(colloid, self.spheres) == False:
                     break
@@ -141,22 +179,22 @@ class ParticleSystem:
 
         print()
 
-    def addSolvent(self, nsolv:int, sigma_solvent:float, solventType:int):
+    def addSolvent(self):
 
-        for nc in range(nsolv):
+        for nc in range(self.nsolvent):
 
-            percent = 100 * (nc + 1) / nsolv
-            print(f"\rSolvent {nc+1}/{nsolv}  ({percent:6.2f}%)", end="")
+            percent = 100 * (nc + 1) / self.nsolvent
+            print(f"\rSolvent {nc+1}/{self.nsolvent}  ({percent:6.2f}%)", end="")
 
             while True:
 
-                solvent = Sphere(sigma_solvent, 
+                solvent = Sphere(self.sigma_solvent, 
                                 (np.random.uniform(-self.LboxHalf[0], self.LboxHalf[0]),
                                 np.random.uniform(-self.LboxHalf[1], self.LboxHalf[1]),
                                 np.random.uniform(-self.LboxHalf[2], self.LboxHalf[2])),
                                 self.atomID,
                                 self.molID,
-                                solventType)
+                                self.solventType)
 
                 if self.atomList.overlapCheck(solvent, self.spheres) == False:
                     break
@@ -169,3 +207,86 @@ class ParticleSystem:
             self.molID += 1
 
         print()
+    
+    def writeLAMMPSfile(self):
+
+        with open(self.par["atomFile"], "w+") as f:
+
+            f.write("LAMMPS data file\n\n")
+
+            f.write(f"{self.ntot} atoms\n")
+            f.write(f"{self.nbonds} bonds\n")
+            f.write("0 angles\n0 dihedrals\n0 impropers\n\n")
+
+            f.write(f"{self.atom_types} atom types\n{self.bond_types} bond types\n0 angle types\n0 dihedral types\n0 improper types\n\n")
+
+            f.write(f"0.0 {self.Lbox[0]} xlo xhi\n0.0 {self.Lbox[1]} ylo yhi\n0.0 {self.Lbox[2]} zlo zhi\n\n")
+
+            f.write(f"Masses\n\n{self.beadType} 1\n")
+
+            if self.nsolvent > 0:
+                f.write(f"{self.solventType} 0.5\n")  #!Solvent mass 0.5 #TODO: read from par file
+
+            if self.npatch > 0:
+                f.write(f"{self.patchType} 1\n")   #!Patch mass 1 #TODO: read from par file
+
+            if self.ncolloids > 0:
+                f.write(f"{self.colloidType} 1\n")     #!Colloid mass 1 #TODO: read from par file
+
+            f.write("\n")
+
+            f.write("Atoms\n")
+            f.write("# atom-ID\tmol-ID\tatom-type\tx\ty\tz\n")
+
+            for sph in self.spheres:
+                f.write(f"{sph.atomID} {sph.molID} {sph.atomType} {sph.cm[0]+self.LboxHalf[0]} {sph.cm[1]+self.LboxHalf[1]} {sph.cm[2]+self.LboxHalf[2]}\n")
+
+            f.write("\n")
+
+
+            # -----------> VELOCITIES <------------- # #*It is possible to specify the atoms velocities
+
+            #f.write("Velocities\n\n")
+
+            #[f.write("{0} {1} {2} {3}\n".format(i, np.random.random() * 2., np.random.random()  * 2., np.random.random()  * 2. - 1.)) for i in range(1, ntot + 1)]
+
+            #f.write("\n")
+
+
+            # -----------> BONDS <------------- #
+
+            # Bond IDs
+            f.write("Bonds\n#bond-ID\tbond-type\tfirst-atom\tsecond-atom\n")
+
+            ID_bond = 1
+
+            #Bonds between the beads
+            for p in range(self.npol):
+
+                #First bead of the polymer
+                ID_start = int(1 + p * (self.ns + self.npatch))
+
+                for i in range(self.ns - 1):
+
+                    atom_id = ID_start + i
+
+                    f.write(f"{ID_bond} 1 {atom_id} {atom_id + 1}\n")
+
+                    ID_bond += 1
+
+            #Bonds between the bead and the patch
+            for BeadID,PatchID in self.patchyBeadsIDs:
+
+                f.write(f"{ID_bond} 2 {BeadID} {PatchID}\n")
+                ID_bond += 1
+
+
+    def saveXYZfile(self) -> None:
+
+        namesVMD = {1: 'C', 2:'H', 3:'O', 4:'N'}
+
+        with open(self.par["xyzFile"], "w+") as f:
+            f.write(f"{self.ntot}\n\n")
+            for sphere in self.spheres:
+                letter = namesVMD[sphere.atomType]
+                f.write(f"{letter} {sphere.cm[0]} {sphere.cm[1]} {sphere.cm[2]}\n")
